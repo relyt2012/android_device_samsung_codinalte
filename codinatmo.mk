683a64c133aa1967fcdec0aa78b076efc2872f0d
Meticulus <theonejohnnyd@gmail.com>
2014-02-28 07:57:44 -0600

    recovery: fix usb init and isolate recovery.
    -This fixes USB issues in recovery.
    
    -This also pulls the j4fs and param modules and puts them
    in the initramfs of recovery to maintian a vermagic match
    with the recovery kernel. The modules are loaded from initramfs
    before system is mounted.
    
    NOTE: Initramfs path /system/lib/modules is hidded after /system
    is mounted.

--- codinatmo.mk
+++ codinatmo.mk
@@ -39,10 +39,12 @@ PRODUCT_COPY_FILES += \
 # Recovery ramdisk, libraries and modules.
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/rootdir/init.recovery.samsungcodina.rc:root/init.recovery.samsungcodina.rc \
-    $(LOCAL_PATH)/recovery/rootdir/etc/twrp.fstab:recovery/root/etc/twrp.fstab \
+    $(LOCAL_PATH)/recovery/rootdir/etc/recovery.fstab:recovery/root/etc/recovery.fstab \
     $(LOCAL_PATH)/recovery/rootdir/sbin/libkeyutils.so:recovery/root/sbin/libkeyutils.so \
     $(LOCAL_PATH)/recovery/rootdir/sbin/libsec_km.so:recovery/root/sbin/libsec_km.so \
-    $(LOCAL_PATH)/recovery/rootdir/sbin/libsec_ecryptfs.so:recovery/root/sbin/libsec_ecryptfs.so
+    $(LOCAL_PATH)/recovery/rootdir/sbin/libsec_ecryptfs.so:recovery/root/sbin/libsec_ecryptfs.so \
+    $(TARGET_PRODUCT_OUT)/system/lib/modules/j4fs.ko:recovery/root/system/lib/modules/j4fs.ko \
+    $(TARGET_PRODUCT_OUT)/system/lib/modules/param.ko:recovery/root/system/lib/modules/param.ko
 
 # Inputs
 PRODUCT_COPY_FILES += \
