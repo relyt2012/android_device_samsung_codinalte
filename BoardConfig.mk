ff6d978bf00032e0261e47ad60859f0ad45bc81e
Meticulus <theonejohnnyd@gmail.com>
2014-03-01 17:59:26 -0600

    recovery: make recovery independant of ROM's
    -The fact that CM puts kernel modules on the system partition and
    Samsung puts them in the initramfs causes recovery to be dependant
    on matching kernel modules in the ROM.
    
    -To solve this we move the modules that recovery needs (param.ko
    & j4fs.ko) to the initramfs of recovery.
    
    -Now there will be no need to flash another recovery to flash
    another ROM.
    
    NOTE: To accomplish this goal I implemented a custom bootimage mk.
    The kernel is still built the exact same way. It was necessary
    to include it in shbootimg.mk.The way the recovery is built has
    changed in the following ways:
    
    Before: recovery and ramdisk were built at the same time and
    when those were done the kernel was build and finally the
    recovery.img.
    
    Now: kernel and recovery are built at the same time and when those
    were done param.ko and j4fs.ko are copied to initramfs, the ramdisk
    is built and finally the recovery.img.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,6 +50,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_codinatmo_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/codinatmo
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/codinatmo/shbootimg.mk
 BOARD_KERNEL_CMDLINE := "androidboot.selinux=permissive"
 TARGET_USE_ST_ERICSSON_KERNEL := true
 BOARD_KERNEL_BASE := 0x00000000
