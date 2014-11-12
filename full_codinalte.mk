fc45c342c5ca59a4aca5e496c046c0a6e02f0f89
Meticulus <theonejohnnyd@gmail.com>
2014-11-12 12:54:49 -0600

    more changes from carbon

--- full_codinalte.mk
+++ full_codinalte.mk
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/codinalte/codinalte.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := carbon_codinalte
+PRODUCT_NAME := aosp_codinalte
 PRODUCT_DEVICE := codinalte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo carbon_codinalte-userdebug
+add_lunch_combo aosp_codinalte-userdebug
