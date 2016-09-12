e0b08815341551084bc78484af2692eeb54d04ca
Meticulus <theonejohnnyd@gmail.com>
2016-09-12 12:01:47 -0500

    device mk: Let ROM define these.
    -Should have gotten rid of these along time ago.

--- cm_codinalte.mk
+++ cm_codinalte.mk
@@ -32,5 +32,5 @@ TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
 # Set build fingerprint and ID
-BUILD_ID := TRIANA00$(shell date -u +%Y%m%d)
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=cm_codinalte BUILD_FINGERPRINT=cyanogenmod/cm_codinalte/codinalte:4.4.2/$(BUILD_ID) PRIVATE_BUILD_DESC="SGH-T599X 4.4.2"
+#BUILD_ID := TRIANA00$(shell date -u +%Y%m%d)
+#PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=cm_codinalte BUILD_FINGERPRINT=cyanogenmod/cm_codinalte/codinalte:4.4.2/$(BUILD_ID) PRIVATE_BUILD_DESC="SGH-T599X 4.4.2"
